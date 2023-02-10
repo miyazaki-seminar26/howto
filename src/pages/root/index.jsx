b822607ec639171bd70b9d154bf29c18d57129d6
Shin.Yamamoto <yamashin0123@outlook.jp>
2023-02-11 03:03:31 +0900

    🐛 buildエラー解消

--- src/pages/root/index.jsx
+++ src/pages/root/index.jsx
@@ -1,7 +1,12 @@
-export const HomePage = () => {
+import { AppLayout } from '@/components/layout/layout';
+import { Home } from '@/components/pages/Home';
+
+const HomePage = () => {
   return (
     <AppLayout>
       <Home />
     </AppLayout>
   );
 };
+
+export default HomePage;
