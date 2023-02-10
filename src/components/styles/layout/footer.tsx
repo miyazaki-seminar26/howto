cbfe66f52a940b6a9b892588c47ee015b6f7abb1
kyoko-furuki <121999760+kyoko-furuki@users.noreply.github.com>
2023-02-11 01:41:25 +0900

    header,footer作ったよ

---
+++ src/components/styles/layout/footer.tsx
@@ -0,0 +1,27 @@
+import { LINKS } from '@/constants/link';
+import { Anchor, Flex, Text } from '@mantine/core';
+import { InstagramIcon } from '../icon/InstagramIcon';
+import { TwitterIcon } from '../icon/TwitterIcon';
+
+export const AppFooter = () => {
+  return (
+    <footer className="flex  py-4">
+      <Flex gap={24}>
+        <Anchor
+          className="hover:opacity-80 transition-opacity"
+          href={LINKS.INSTAGRAM}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </Anchor>
+        <Anchor className="hover:opacity-80 transition-opacity">
+          <TwitterIcon />
+        </Anchor>
+        <Text color="white" size="xs" className="flex items-center">
+          ©️miyazaki-seminar 2023
+        </Text>
+      </Flex>
+    </footer>
+  );
+};
---
+++ src/components/styles/layout/header.tsx
@@ -0,0 +1,65 @@
+import { LINKS } from '@/constants/link';
+import { Anchor } from '@mantine/core';
+import { useRouter } from 'next/router';
+import { FC } from 'react';
+
+type MenuType = {
+  label: string;
+  href: string;
+}[];
+
+const MENU: MenuType = [
+  {
+    label: 'HOME',
+    href: LINKS.HOME,
+  },
+  {
+    label: 'PROJECT',
+    href: LINKS.PROJECT,
+  },
+  {
+    label: 'ABOUT US',
+    href: LINKS.ABOUTUS,
+  },
+  {
+    label: 'FAQ',
+    href: LINKS.FAQ,
+  },
+];
+
+type Props = {
+  path: string;
+  label: string;
+};
+
+const AppLink: FC<Props> = ({ path, label }) => {
+  const pathname = useRouter().pathname;
+  return (
+    <Anchor
+      variant="text"
+      href={path}
+      color="dark"
+      className={`hover:opacity-80 transition-all ${
+        pathname === path && 'font-bold underline'
+      }`}
+    >
+      {label}
+    </Anchor>
+  );
+};
+
+export const AppHeader: FC = () => {
+  return (
+    <header>
+      <nav>
+        <ul className="flex gap-4 md:gap-8 text-[15px] md:text-xl">
+          {MENU.map(item => (
+            <li key={item.label}>
+              <AppLink path={item.href} label={item.label} />
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
