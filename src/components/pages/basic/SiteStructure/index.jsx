8e550e3fc916eedc2b1326d973459a5d5f0ddb1e
39TO <ohisama.0610@icloud.com>
2023-02-12 01:04:32 +0900

    fix

--- src/components/pages/basic/SiteStructure/index.jsx
+++ src/components/pages/basic/SiteStructure/index.jsx
@@ -1,14 +1,10 @@
 import { PageHeader } from '@/components/parts/PageHeader';
 import { PATHS } from '@/constants/link';
-import { OUTLINE } from '@/constants/outline';
 
 export const SiteStructure = () => {
   return (
     <div>
-      <PageHeader
-        pageTitle={PATHS.SITE_STRUCTURE.name}
-        outline={OUTLINE.SITE_STRUCTURE}
-      />
+      <PageHeader pageTitle={PATHS.SITE_STRUCTURE.name} />
     </div>
   );
 };
