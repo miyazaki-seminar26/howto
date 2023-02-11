5896a2ede9773fc4f200e98b4914b7a88a29f233
tokumoto-jin <cgeg0826@mail3.doshisha.ac.jp>
2023-02-11 12:41:44 +0900

    第5フェーズを作りました。

---
+++ src/components/pages/basic/Practice2/index.jsx
@@ -0,0 +1,7 @@
+export const Practice2 = () => {
+  return (
+    <div>
+      <div>Practice2</div>
+    </div>
+  );
+};
---
+++ src/pages/basic/practice3/index.jsx
@@ -0,0 +1,12 @@
+import { AppLayout } from '@/components/layout/layout';
+import { Practice3 } from '@/components/pages/basic/Practice3';
+
+const Practice3Page = () => {
+  return (
+    <AppLayout>
+      <Practice3 />
+    </AppLayout>
+  );
+};
+
+export default Practice3Page;
