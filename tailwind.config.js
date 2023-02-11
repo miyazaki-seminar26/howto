de8e271c9d5ee46fdde34f5029678dffa2bd9825
tokumoto-jin <cgeg0826@mail3.doshisha.ac.jp>
2023-02-12 02:01:13 +0900

    PageHeaderのコンポーネントをちょっと作りました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   content: ['./pages/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '20': '20px'
+    }
   },
   corePlugins: {
     preflight: false, // リセットCSSの無効化
