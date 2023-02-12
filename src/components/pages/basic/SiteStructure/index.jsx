72fbc0a069de260b988488a94c379306fb238b1f
tokumoto-jin <cgeg0826@mail3.doshisha.ac.jp>
2023-02-12 13:03:22 +0900

    診断結果ページに飛ぶリンクを作りました。

--- src/components/pages/basic/SiteStructure/index.jsx
+++ src/components/pages/basic/SiteStructure/index.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 
+import Link from 'next/link';
+
+import { IconArrowNarrowRight } from '@tabler/icons-react';
+
 import { ArrowNav } from '@/components/parts/ArrowNav';
 import { BellText } from '@/components/parts/BellText';
 import { PageHeader } from '@/components/parts/PageHeader';
@@ -17,6 +21,18 @@ export const SiteStructure = () => {
       <BellText text="サイト構造は、サイトの構成要素をどのように配置するかを決めることです。サイト構造は、サイトの構成要素をどのように配置するかを決めることです。サイト構造は。" />
       <button onClick={handleClick}>click</button>
       <div>クリック数{state}</div>
+      <Link href="#">
+        <div className="flex items-center justify-center hover:text-blue-500">
+          <IconArrowNarrowRight size={40} strokeWidth={0.5} color={'black'} />
+          x点以上の方はこちら
+        </div>
+      </Link>
+      <Link href="#">
+        <div className="flex items-center justify-center hover:text-blue-500">
+          <IconArrowNarrowRight size={40} strokeWidth={0.5} color={'black'} />
+          x点以下の方はこちら
+        </div>
+      </Link>
       <ArrowNav previousPage={PAGES.HOME} nextPage={PAGES.HOME} />
     </div>
   );
